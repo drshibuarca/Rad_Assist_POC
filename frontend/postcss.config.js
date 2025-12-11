8d97882f825eee34c885fb723cb628b56c48e359
drshibuarca <dr.shibu@arcaai.com>
2025-12-11 09:10:24 +0530

    Add files via upload

---
+++ frontend/postcss.config.js
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+export default {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+}
+=======
+export default {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+}
+>>>>>>> 533fb4b11c272a6a418375a1b2219f1c766b90bb
